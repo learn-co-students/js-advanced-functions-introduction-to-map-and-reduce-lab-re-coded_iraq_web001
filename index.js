9535bd09527ab1200af97a533f43ea28f4c9c61d
Zainab Azzam <zainab_azzam@yahoo.com>
2020-06-12 10:58:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Your code here
+ const mapToNegativize = function (sourceArray){
+   let result = sourceArray.map(x => x* -1)
+   return result
+ }
+ const mapToNoChange = function (sourceArray){
+  let result = sourceArray.map(x => x)
+   return result
+   
+ }
+  const mapToDouble = function (sourceArray){
+  let result = sourceArray.map(x => x*2)
+   return result
+   
+ }
+  const mapToSquare = function (sourceArray){
+  let result = sourceArray.map(x => Math.pow(x, 2))
+   return result
+   
+ }
+ 
+ function reduceToTotal(sourceArray, startingPoint = 0){
+    const reducer = function(accumulator, currentValue){ return accumulator + currentValue }
+
+    return sourceArray.reduce(reducer, startingPoint)
+}
+ function reduceToAllTrue(sourceArray){
+
+    const reducer = function(accumulator, currentValue){  
+        if(!!accumulator == true && !!currentValue == true){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    return sourceArray.reduce(reducer, true)
+}
+
+function reduceToAnyTrue(sourceArray){
+    const reducer = function(accumulator, currentValue){  
+        if(accumulator == true){
+            return true
+        } else {
+            return !!currentValue
+        } 
+    }
+    return sourceArray.reduce(reducer, false)
+} 
